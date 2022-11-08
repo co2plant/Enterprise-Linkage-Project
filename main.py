bf5e59ea6cce0945fb457b57bb050a8cc815e017
co2plant's window <jaonz6057@wku.ac.kr>
2022-11-09 01:23:19 +0900

    latest update for live sharing

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import time
 import tkinter
 
-from PIL import ImageGrab
 from asyncio.windows_events import NULL
 from hashlib import new
 from overlay import Overlay
@@ -30,8 +29,10 @@ ocrs = Tesseract_Ocr()
 looptime = time.time()
 
 #------------------------------variable------------------------------
-
+global overlay_screen_1
+global overlay_screen_2
 #--------------------------------------------------------------------
+
 #--------------------------------------------------------------------
 
 def button1_pressed():
@@ -46,45 +47,20 @@ def button1_pressed():
 #--------------------------------------------------------------------
 
 def while_loop():
-    
-    #global overlay_screen_2
-    global overlay_screen_1
-    #global overlay_switch
-    overlay_screen_1 = Overlay(frame1)
-    #overlay_screen_2 = Overlay(frame2)
     screenshot = caps.get_screenshot()
     result, str_result = ocrs.Get_Ocr_Tesseract(screenshot)
     for i in range(1, len(result["text"])):
         text = result["text"][i]
         conf = int(result["conf"][i])
     
-        # while y++, until another cracter is come
-        
-        if(conf>70):
-            tmptext = "".join([c if ord(c)<128 else "" for c in text]).strip()
-            print(str_result)
-            tmptext = translate.GetTranslate(tmptext, 'ko', 'en')
+        if((conf>70) and (result["left"][i-1]+result["width"][i-1]<result["left"][i])):
+            #tmptext = "".join([c if ord(c)<128 else "" for c in text]).strip()
+            #tmptext = translate.GetTranslate(tmptext, 'ko', 'en')
+            tmptext = " ".join(result["text"][i])
             arr=caps.get_rect()#arr[0] = x, arr[1]=y
             overlay_screen_1.labeler(tmptext,result["left"][i]+arr[0]+8,result["top"][i]+arr[1]+30, result["width"][i], result["height"][i])
-            #overlay_screen_2.labeler(tmptext,result["left"][i],result["top"][i])
-            
-            #tmptext = NULL
-            #tmptext = Translate.Trans(tmptext, eng, kor)
     print(str_result)
-    print(translate.GetTranslate(str_result, 'ko', 'en'))
-    """
-    if(overlay_switch == True):
-        overlay_screen_1.stop()
-        print("overlay_1")
-    else:
-        overlay_screen_2.stop()
-        print("overlay_2")
-    overlay_switch = not overlay_switch
-    """
-    #overlay_screen_1.win.destroy()
     frame1.after(1000,while_loop)
-    
-
 
 #--------------------------------------------------------------------
 
@@ -96,14 +72,10 @@ frame1 = tkinter.Tk()
 frame1.title("Bridge - OCR Translator")
 frame1.geometry("640x400+100+100")
 #frame2 = tkinter.Tk()
-global overlay_screen_1
 overlay_screen_1 = Overlay(frame1)
 
 #--------------------------------------------------------------------
-#global overlay_screen_2
-#overlay_screen_2 = Overlay(frame2)
-#global overlay_switch
-#overlay_switch = False
+
 #button1 = tkinter.Button(frame1, text = 'Start OCR', background='white')
 #button1.pack()
 #button1.config(command=button1_pressed)
--- ocr.py
+++ ocr.py
@@ -2,20 +2,16 @@ import numpy as np
 import pytesseract
 import cv2
 import matplotlib.pyplot as plt
-import overlay
 
 from pytesseract import Output
 from overlay import Overlay
 
 
 class Tesseract_Ocr():
-        
-    def __init__(self):
-        str_i=""
 
     def Get_Ocr_Tesseract(self, screenshot):
         screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
 
-        str_result = pytesseract.image_to_string(screenshot, lang='eng+kor')
+        str_result = pytesseract.image_to_string(screenshot, lang='eng')#+kor
         result = pytesseract.image_to_data(screenshot, output_type=Output.DICT) # 테서렉트에서 좌표값 뽑아옴
         return result, str_result
